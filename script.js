6ec894dad6f04f292e0fb7db14391cce321035e1
Adam Troxell <adamtxl@hotmail.com>
2024-05-06 16:48:23 -0500

    Variables are set up for attack options and buttons are set to subtract from the variables

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+let shroomHP = 100;
+let myAP = 100;
+// let shroomHP = document.querySelector('hp-text').innerHTML;
+// let myAP = document.getElementById('ap-meter').innerHTML;
+const arcaneScepterStats = {
+    damage: 14,
+    apCost: 12
+}
+const entangleStats = {
+    damage: 9,
+    apCost: 23
+}
+const dragonBladeStats = {
+    damage: 47,
+    apCost: 38
+}
+const starFireStats = {
+    damage: 25,
+    apCost: 33
+}
 
 function onReady() {
     console.log("Ready to go!")
@@ -18,3 +38,31 @@ function onReady() {
 
 
 onReady()
+
+function arcaneScepterAttack (event) {
+    console.log('Arcane Scepter attack!')
+    shroomHP -= arcaneScepterStats.damage;
+    myAP -= arcaneScepterStats.apCost;
+    console.log('You dealt 14 damage with your scepter. Enemy has', shroomHP, ' left. You have' , myAP, 'attack points remaining');
+}
+
+function entangleAttack(event) {
+    console.log('Entangling action!')
+    shroomHP -= entangleStats.damage;
+    myAP -= entangleStats.apCost;
+    console.log('You entangled the enemy! Enemy has ', shroomHP, 'health remaining. You have ', myAP, 'attack points remaining')
+}
+function dragonBladeAttack(event) {
+    console.log('Attacked with the Dragon Blade!')
+    shroomHP -= dragonBladeStats.damage;
+    myAP -= dragonBladeStats.apCost;
+    console.log('You smote the enemy with your Dragon Blade! Enemy has ', shroomHP, 'Remaining. You have ', myAP, 'attack pointsremaining')
+
+}
+function starFireAttack(event) {
+    console.log('Launched Star Fire!')
+    shroomHP -= starFireStats.damage;
+    myAP -= starFireStats.apCost;
+    console.log('You unleased Star Fire! Enemy has ', shroomHP, 'Remaining. You have ', myAP, 'attack points remaining')
+
+}
